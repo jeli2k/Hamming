42ed185e9d8f6100181feb43ca96c1711b71c234
Fabian Jelinek <wi22b067@technikum-wien.at>
2023-06-13 11:53:29 +0200

    first commit

---
+++ src/main/java/com/example/hemming/HemmingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hemming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemmingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemmingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hemming/HemmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hemming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HemmingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
